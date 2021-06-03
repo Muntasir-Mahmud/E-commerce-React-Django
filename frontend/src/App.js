1ec874bf5b94411af493cb42bc58f38dca5d885f
Muntasir Mahmud <m.siyam529@gmail.com>
2021-06-03 22:05:50 +0600

    Setup bootstrap , added Header Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      My app
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
