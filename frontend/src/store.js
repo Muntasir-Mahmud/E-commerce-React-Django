e24bb056e6ef537e6ed18521547e156364b91d05
Muntasir Mahmud <m.siyam529@gmail.com>
2021-07-11 18:04:34 +0600

    Added cartItem in local storage and in redux (frontend)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,21 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const reducer = combineReducers({
      productList: productListReducer,
-     productDetails : productDetailsReducer,
+     productDetails: productDetailsReducer,
+     cart: cartReducer
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+     JSON.parse(localStorage.getItem('cartItems')) : [];
+
+const initialState = {
+     cart: { cartItems: cartItemsFromStorage }
+}
 
 const middleware = [thunk]
 
