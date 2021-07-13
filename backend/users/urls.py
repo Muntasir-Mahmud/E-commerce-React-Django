21c2b88cd66f5226bd30dd5b57d37bf4049c0b6b
Muntasir Mahmud <m.siyam529@gmail.com>
2021-07-14 03:28:55 +0600

    Customized JWT (backend)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainView.as_view(), name='token_obtain_pair'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+
+class MyTokenObtainSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        return data
+
+class MyTokenObtainView(TokenObtainPairView):
+    serializer_class = MyTokenObtainSerializer
