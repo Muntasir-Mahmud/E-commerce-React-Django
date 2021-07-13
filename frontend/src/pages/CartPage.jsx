9d4514019368202d7746a88407c37e0366f5e12b
Muntasir Mahmud <m.siyam529@gmail.com>
2021-07-13 18:40:14 +0600

    Added Cart remove and fix some bug (frontend)

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react';
 import { Button, Card, Col, Form, Image, ListGroup, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import Message from '../components/Message';
 
 function CartPage({ match, location, history }) {
 
     const productId = match.params.id;
-    const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+    const qty = location.search ? Number(location.search.split('%')[1]) : 1;
 
     const dispatch = useDispatch()
 
@@ -24,7 +24,7 @@ function CartPage({ match, location, history }) {
 
 
     const removeFromCartHandler = (id) => {
-        console.log(id)
+        dispatch(removeFromCart(id))
     }
 
     const checkoutHandler = () => {
@@ -59,7 +59,7 @@ function CartPage({ match, location, history }) {
                                             <Form.Control
                                                 as="select"
                                                 value={item.qty}
-                                                onChange={(e) => dispatch(addToCart(item.product, Number(e.targrt.value)))}
+                                                onChange={(e) => dispatch(addToCart(item.productId, Number(e.target.value)))}
                                             >
                                                 {
                                                     [...Array(item.countInStock).keys()].map((x) => (
