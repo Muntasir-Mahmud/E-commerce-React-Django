16eaabc093b5f7d8da6ef05a1b578596743fd9b8
Muntasir Mahmud <m.siyam529@gmail.com>
2021-07-14 03:42:02 +0600

    Added profile api (backend)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainView.as_view(), name='token_obtain_pair'),
+    path('users/profile/', views.get_user_profile, name='get profile'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,10 @@
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import UserSerializer
 
 
 class MyTokenObtainSerializer(TokenObtainPairSerializer):
@@ -11,3 +16,10 @@ class MyTokenObtainSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainView(TokenObtainPairView):
     serializer_class = MyTokenObtainSerializer
+
+
+@api_view(['GET'])
+def get_user_profile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
