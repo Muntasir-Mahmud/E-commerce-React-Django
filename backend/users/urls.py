e0bd790f690a78725d4a7a5ba12dea4c2f5fd743
Muntasir Mahmud <m.siyam529@gmail.com>
2021-07-14 23:49:59 +0600

    Added permission classes (backend)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('users/login/', views.MyTokenObtainView.as_view(), name='token_obtain_pair'),
     path('users/profile/', views.get_user_profile, name='user_profile'),
+    path('users/', views.get_users, name='users'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -20,7 +21,17 @@ class MyTokenObtainView(TokenObtainPairView):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_user_profile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
