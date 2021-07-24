4677d328ff0fe491df8ddd7118d3f47f38134550
Muntasir Mahmud <m.siyam529@gmail.com>
2021-07-25 00:06:41 +0600

    Added user profile update (backend)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('users/', views.get_users, name='users'),
     path('users/login/', views.MyTokenObtainView.as_view(), name='token_obtain_pair'),
     path('users/profile/', views.get_user_profile, name='user_profile'),
+    path('users/profile/update/', views.update_user_profile, name='user_profile_update'),
     path('users/register/', views.register_user, name='user_register'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -47,6 +47,24 @@ def get_user_profile(request):
     return Response(serializer.data)
 
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_user_profile(request):
+    user = request.user
+    data = request.data
+    serializer = UserSerializerWithToken(user, many=False)
+    
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+    
+    user.save()
+
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
