3ab956262f433217689ed597e03d9cf04c8d1f56
Muntasir Mahmud <m.siyam529@gmail.com>
2021-07-30 20:37:03 +0600

    Added migrations for order models (backend)

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 
-from .models import Order, OrderItem, Product, Review, ShippingAddress
+from .models import Product, Review
 
 admin.site.register(Product)
 admin.site.register(Review)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
